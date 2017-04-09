15dc409cc32e262d3615ef2c903d2c7fd2c82440
mmadhurai <maddylali@gmail.com>
2017-04-09 22:09:03 +0530

    added color screen

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,7 +1,9 @@
 import {Routes} from '@angular/router';
 import {DoorsComponent} from './doors/doors.component';
+import {ColorComponent} from "./color/color.component";
 
 export const appRoutes: Routes = [
   {path: 'doors', component: DoorsComponent},
+  {path: 'color', component: ColorComponent},
   {path: '', redirectTo: '/doors', pathMatch: 'full'}
 ];
