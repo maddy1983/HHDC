ee90e6ec0a9b19059e6380173b8b0b188f77f856
mmadhurai <maddylali@gmail.com>
2017-04-19 17:47:12 +0530

    Added popup

---
+++ src/app/modals/service-repair/service-repair.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-service-repair',
+  templateUrl: './service-repair.component.html',
+  styleUrls: ['./service-repair.component.less']
+})
+export class ServiceRepairComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
