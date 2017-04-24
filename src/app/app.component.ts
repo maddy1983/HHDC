dfc9755dbaae5dc35e3a751e5d137b6469e6e015
mmadhurai <maddylali@gmail.com>
2017-04-24 19:16:02 +0530

    Merge branch 'master' into newBranches
    # Conflicts:
    #	src/app/category/category.component.html
    resolved conflicts
    added content to new screens
    worked on nav styles
    added popup in size screen
    added video

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, AfterViewInit} from '@angular/core';
 import {Location} from '@angular/common';
 import {Router} from '@angular/router';
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewInit {
 
   constructor(private route:Router, private location:Location) {
   }
@@ -35,6 +37,14 @@ export class AppComponent implements OnInit {
     "/config/doorConfiguration",
     "/thankyou"
   ];
+
+  ngAfterViewInit() {
+    var tag = document.createElement('script');
+
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+  }
 
   ngOnInit() {
     this.currScreen = this.navElems.indexOf(this.location.path());
